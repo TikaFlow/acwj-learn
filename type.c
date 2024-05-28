74e90397de690b5f46846e653b4eec4b8d5132a2
TikaFlow <tika_flow@qq.com>
2024-05-28 10:23:32 +0800

    part 22: no new code

--- type.c
+++ type.c
@@ -19,41 +19,6 @@ int is_ptr(int type) {
     return FALSE;
 }
 
-int type_compatible(int *left, int *right, int onlyright) {
-
-    if (*left == *right) {
-        *left = *right = A_NONE;
-        return TRUE;
-    }
-
-    int left_size = gen_type_size(*left), right_size = gen_type_size(*right);
-
-    if (left_size == 0 || right_size == 0) {
-        return FALSE;
-    }
-
-    if (left_size < right_size) {
-        *left = A_WIDEN;
-        *right = A_NONE;
-
-        return TRUE;
-    }
-
-    if (left_size > right_size) {
-        if (onlyright) {
-            return FALSE;
-        }
-
-        *left = A_NONE;
-        *right = A_WIDEN;
-
-        return TRUE;
-    }
-
-    *left = *right = A_NONE;
-    return TRUE;
-}
-
 int pointer_to(int type) {
     int new_type;
 
